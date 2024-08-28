d34cdb228f7d57f8f991aa6671a7d89d59f71f60
MidHunterX <midhunterx@gmail.com>
2024-08-28 19:55:48 +0530

    🧜 Merged: interface into gamedetails

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { Gamedetails } from '../gamedetails';
+
+// {"id":"146","year":"2010","img":"BS2","game":"BioShock 2"},
+export interface Gamedetails {
+  id: number;
+  year: number;
+  img: string;
+  game: string;
+}
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
   gamedetails: Gamedetails = {
-    id:146,
-    year:2010,
-    img:"BS2",
-    game:"BioShock 2",
-  }
+    id: 146,
+    year: 2010,
+    img: 'BS2',
+    game: 'BioShock 2',
+  };
 }
--- src/app/gamedetails.ts
+++
@@ -1,8 +0,0 @@
-// {"id":"146","year":"2010","img":"BS2","game":"BioShock 2"},
-
-export interface Gamedetails {
-  id: number;
-  year: number;
-  img: string;
-  game: string;
-}
