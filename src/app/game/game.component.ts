55fae3e061f8fa8a02a139af70925aa6a36e88a4
MidHunterX <midhunterx@gmail.com>
2024-08-29 16:33:23 +0530

    👌 Feat: Load from JSON with fetch API

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GameInterface } from '../game.interface';
+import {CommonModule} from '@angular/common'; // *ngFor
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
-export class GameComponent {
-  gamedetails: GameInterface = {
-    id: 146,
-    year: 2010,
-    img: 'BS2',
-    game: 'BioShock 2',
-  };
+export class GameComponent implements OnInit {
+
+  games: GameInterface[] = [];
+  ngOnInit(): void {
+    fetch('public/data.json')
+      .then(response => response.json())
+      .then((data: GameInterface[]) => {
+        this.games = data;
+      })
+      .catch(error => console.error('Error loading GameData:', error));
+  }
 }
