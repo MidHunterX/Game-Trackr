3d2a1ed1dab522863e47891edd31611891f1291d
MidHunterX <midhunterx@gmail.com>
2024-08-31 09:46:11 +0530

    🚀 Added: Game Details Component and Route

---
+++ src/app/game-details/game-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GameItemInterface } from '../gameItem.interface';
+import { GameDataService } from '../game-data.service';
+
+@Component({
+  selector: 'app-game-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-details.component.html',
+  styleUrl: './game-details.component.scss',
+})
+export class GameDetailsComponent implements OnInit {
+  game: GameItemInterface | undefined;
+
+  constructor(
+    private route: ActivatedRoute, // for route parameters (:id)
+    private router: Router,
+    private gameDataService: GameDataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.game = this.gameDataService.getSelectedGame();
+    console.log("log: Recieved on /game-details: " + this.game)
+    if (!this.game) {
+      this.router.navigate(['/']);
+    }
+  }
+}
