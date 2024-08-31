4a81bcb5516ad166bc91fcf5e66d3bdcdc4c65f6
MidHunterX <midhunterx@gmail.com>
2024-08-31 09:46:11 +0530

    🚀 Added: Game Details Component and Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'game-details/:id', component: GameDetailsComponent },
+];
