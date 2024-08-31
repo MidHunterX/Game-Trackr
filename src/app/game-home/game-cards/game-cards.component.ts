e47c099d7e53dc8c75e5a7fbc76f43760a059593
MidHunterX <midhunterx@gmail.com>
2024-08-31 10:37:40 +0530

    🚀 Added: Routes for Recently Played

--- src/app/game-home/game-cards/game-cards.component.ts
+++ src/app/game-home/game-cards/game-cards.component.ts
@@ -23,7 +23,7 @@ export class GameCardsComponent {
 
   viewDetails(game: GameItemInterface): void {
     this.gameDataService.setSelectedGame(game);
-    console.log("log: Sending data to /game-details: " + game)
+    console.log('log: Sending data to /game-details: ' + game);
     this.router.navigate(['/game-details', game.id]);
   }
 }
