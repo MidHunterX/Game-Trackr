f4effebc8d80d6cc6bc6798f6a6268adbf814195
MidHunterX <midhunterx@gmail.com>
2024-08-31 10:33:21 +0530

    🏡 Added: Game Home Component for Route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // *ngFor
 import { RouterOutlet } from '@angular/router';
-import { GameItemInterface } from './gameItem.interface';
-import { GameCardsComponent } from './game-cards/game-cards.component';
-import { RecentlyPlayedComponent } from './recently-played/recently-played.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    GameCardsComponent,
-    RecentlyPlayedComponent,
-  ],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Played-Games-List';
-  games: GameItemInterface[] = [];
-  recentlyPlayed: GameItemInterface[] = [];
-  remainingGames: GameItemInterface[] = [];
-
-  ngOnInit(): void {
-    fetch('public/data.json')
-      .then((response) => response.json())
-      .then((data: GameItemInterface[]) => {
-        this.games = data;
-        this.recentlyPlayed = this.games.slice(0, 2);
-        this.remainingGames = this.games.slice(2);
-      })
-      .catch((error) => console.error('Error loading GameData:', error));
-  }
 }
