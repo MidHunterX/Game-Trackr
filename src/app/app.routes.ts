a07b52590cee346e5d2b2cf5fae4a3189ff36e2a
MidHunterX <midhunterx@gmail.com>
2024-08-31 10:33:21 +0530

    🏡 Added: Game Home Component for Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
+import { GameHomeComponent } from './game-home/game-home.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: GameHomeComponent },
   { path: 'game-details/:id', component: GameDetailsComponent },
 ];
