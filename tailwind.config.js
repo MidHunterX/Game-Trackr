f685e4ad461df1c3d0eaf66f5e4f5ed9ef5032e0
MidHunterX <midhunterx@gmail.com>
2024-09-20 20:16:01 +0530

    👮 Force Tailwind Dark Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,ts}",
   ],
@@ -9,4 +10,7 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: false,
+  },
 }
